9b1961ecb5346e5b4e586316f3fd2267210d3053
RamS24 <AlSaadi.Rami24@gmail.com>
2022-10-11 15:21:09 +0200

    sol

--- src/index.js
+++ src/index.js
@@ -1,7 +1,13 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
+import BootstrapCard from "./components/bootstrap-card/BootstrapCard";
 
 //create your function here
 
 //remember to use ReactDOM.render to include the component into the website
 
+const root = ReactDOM.createRoot(document.getElementById('myDiv'));
+root.render(<BootstrapCard />);
+
+
+// ReactDOM.render(<BootstrapCard />, document.getElementById('myDiv'));
